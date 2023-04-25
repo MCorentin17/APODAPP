978f20f8d67240efdc50135a8acd0f1e371bbd06
Corentin MONVILLERS <c.monvillers@live.fr>
2023-04-25 18:20:04 +0200

    reverse list + pictures of month

--- components/Feed.js
+++ components/Feed.js
@@ -11,7 +11,7 @@ export default function Feed() {
     {
       id: 0,
       url:
-        "https://api.nasa.gov/planetary/apod?api_key=oSYEis9hTQWp8hcqfoR1wPqkcoAxQcQGEYUhsiO2",
+      "https://api.nasa.gov/planetary/apod?api_key=oSYEis9hTQWp8hcqfoR1wPqkcoAxQcQGEYUhsiO2&concept_tags=True",
       date: "",
     },
   ]);
@@ -32,7 +32,7 @@ export default function Feed() {
         id: index,
         url: item.url,
         title: item.title,
-        date: item.date, // Ajout de la propriété date
+        date: item.date, 
       }));
       setImgList(newImgList);
       // Création d'une référence pour chaque image
@@ -42,11 +42,14 @@ export default function Feed() {
     });
   };
 
-  // Fonction pour afficher la liste d'images
-  const renderImgList = () => {
-    return imgList.map((img, index) => (
-        <React.Fragment key={img.id}>
-          <Text style={styles.title}> {img.title}</Text>
+ // Fonction pour afficher la liste d'images
+const renderImgList = () => {
+    // Utilisez la méthode .reverse() pour inverser l'ordre des éléments dans le tableau
+    const reversedImgList = [...imgList].reverse();
+  
+    return reversedImgList.map((img, index) => (
+      <React.Fragment key={img.id}>
+        <Text style={styles.title}> {img.title}</Text>
         <Image
           ref={imgRefs.current[index]}
           source={{ uri: img.url }}
--- data/Api.js
+++ data/Api.js
@@ -1,7 +1,7 @@
 //  Call Api for feed of Pictures
 export function Api() {
     return fetch(
-      "https://api.nasa.gov/planetary/apod?api_key=oSYEis9hTQWp8hcqfoR1wPqkcoAxQcQGEYUhsiO2&count=20&concept_tags=True"
+      "https://api.nasa.gov/planetary/apod?api_key=oSYEis9hTQWp8hcqfoR1wPqkcoAxQcQGEYUhsiO2&start_date=2023-04-01&concept_tags=True&hd=True"
     ).then((res) => res.json());
   }
 

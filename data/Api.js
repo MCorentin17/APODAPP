a2a90bf582a04724dc55b1e7740ba49987d2aec2
Corentin MONVILLERS <c.monvillers@live.fr>
2023-04-27 09:17:11 +0200

    file .env

--- data/Api.js
+++ data/Api.js
@@ -1,8 +1,11 @@
+import {API_KEY} from '@env';
+
 // Call to NASA's API 
 export async function Api() {
+  const apiKey = API_KEY
   try {
     const response = await fetch(
-      `https://api.nasa.gov/planetary/apod?api_key=oSYEis9hTQWp8hcqfoR1wPqkcoAxQcQGEYUhsiO2&start_date=2023-04-01&concept_tags=True&hd=True`
+      `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&start_date=2023-04-01&concept_tags=True&hd=True`
     );
     const data = await response.json();
     return data;
